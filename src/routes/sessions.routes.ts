93e1ee1d5c121866f6513a8685c9b4a7b3d19bae
Lincoln Lopes <lincolnlopes@msn.com>
2021-03-07 22:10:28 -0300

    refactor:centrlização dos exceptions;

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,23 +5,19 @@ import AuthenticateUserServices from '../services/AuthenticateUserService';
 const sessstionsRouter = Router();
 
 sessstionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticateUserServices = new AuthenticateUserServices();
+  const authenticateUserServices = new AuthenticateUserServices();
 
-    const { user, token } = await authenticateUserServices.execute({
-      email,
-      password,
-    });
+  const { user, token } = await authenticateUserServices.execute({
+    email,
+    password,
+  });
 
-    //@ts-expect-error
-    delete user.password;
+  //@ts-expect-error
+  delete user.password;
 
-    return response.json({ user, token });
-  } catch (err) {
-    return response.status(err.statusCode).json({ message: err.message });
-  }
+  return response.json({ user, token });
 });
 
 export default sessstionsRouter;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -9,20 +9,16 @@ const userRouter = Router();
 const upload = multer(uploadConfig);
 
 userRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({ name, email, password });
+  const user = await createUser.execute({ name, email, password });
 
-    //@ts-expect-error
-    delete user.password;
+  //@ts-expect-error
+  delete user.password;
 
-    return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ message: err.message });
-  }
+  return response.json(user);
 });
 
 userRouter.patch(
@@ -30,23 +26,17 @@ userRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    //console.log(request.file);
-    //return response.json({ ok: true });
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
+    const updateUserAvatar = new UpdateUserAvatarService();
 
-      const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-      });
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
 
-      //@ts-expect-error
-      delete user.password;
+    //@ts-expect-error
+    delete user.password;
 
-      return response.json(user);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
+    return response.json(user);
   },
 );
 
